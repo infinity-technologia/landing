aec813a98c1e4c09408eb5e2d71f6fcb7d55fcf7
miartayasa <miartayasa10@gmail.com>
2024-02-13 20:47:56 +0800

    v1 realse

--- app/Livewire/Service.php
+++ app/Livewire/Service.php
@@ -2,10 +2,12 @@
 
 namespace App\Livewire;
 
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
 class Service extends Component
 {
+    #[Title('Services - ')]
     public function render()
     {
         return view('livewire.service');
